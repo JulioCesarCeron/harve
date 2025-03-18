bf4eca75d1e2688509afa84f3f79cb293c5c4e20
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2025-03-17 23:23:38 -0300

    Bootcamp aula 1

---
+++ aula1/bootcamp.js
@@ -0,0 +1,25 @@
+// const text = "Feliz aniverário João!"
+
+// const person = "Maria"
+// console.log("Feliz anivesário " + person)
+// console.log(`Feliz anivesário ${person}`)
+
+let a = 10
+let b = 20
+const c = null
+
+const message1 = `A: ${a}\nB: ${b}\nC: ${c}`
+
+// swap: 10
+// a: 10
+const swap = a
+
+// a: 20
+a = b
+
+// b: 10
+b = swap
+
+const message2 = `A: ${a}\nB: ${b}\nC: ${c}`
+
+console.log(`${message1}\n-----------\n${message2}`)
