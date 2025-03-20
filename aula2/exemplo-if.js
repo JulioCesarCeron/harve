bd07ed30c788b8695022d9ada7da32a3d6937ebd
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2025-03-19 23:22:36 -0300

    Exercícios da aula2

--- aula2/exemplo-if.js
+++ aula2/exemplo-if.js
@@ -5,6 +5,7 @@ const averageElement = document.getElementById("average")
 const faultElement = document.getElementById("fault")
 const faultsElement = document.getElementById("faults")
 const resultElement = document.getElementById("result")
+const nameElement = document.getElementById("name")
 
 let sumNotes = 0
 let sumFaults = 0
@@ -14,6 +15,12 @@ buttonElement.addEventListener("click", () => {
   sumNotes = sumNotes + Number(note)
 
   const fault = faultElement.value
+
+  // Como descobrir o tipo de uma variavel
+  console.log('valor vindo do html "fault" ', typeof fault)
+  const faultNumber = Number(fault)
+  console.log("valor convertido 'fault'", typeof faultNumber)
+
   sumFaults = sumFaults + Number(fault)
 
   listNotesElement.innerHTML += `<li>${note}</li>`
@@ -34,13 +41,19 @@ buttonElement.addEventListener("click", () => {
     //   alert("Reprovado")
     // }
 
+    // printar na tela o total de faltas
+    faultsElement.innerHTML += `<hr>`
+    faultsElement.innerHTML += `<p>Total de faltas: ${sumFaults}</p>`
+
     const louvorMessage =
       average === 10 && sumFaults === 0
-        ? `<p style="color: green; text-transform: uppercase">Aprovado com louvor</p>`
+        ? `<p>${nameElement.value} tirou a média ${average} e não teve nenhuma falta. Parabéns ${nameElement.value}, <span style="color: green; text-transform: uppercase">Aprovado com louvor</span><p>`
         : false
 
     if (louvorMessage) {
       resultElement.innerHTML += louvorMessage
+    } else if (average >= 5 && average < 7 && sumFaults <= 10) {
+      resultElement.innerHTML += `<p style="color: yellow; text-transform: uppercase">Recuperação</p>`
     } else {
       const message = average >= 7 && sumFaults <= 10 ? "Aprovado" : "Reprovado"
       alert(message)
